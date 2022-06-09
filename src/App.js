c07ddd4e690f9b8aff194939dad081cf68e0480d
sebli <seblewongelabera@gmail.com>
2022-06-09 15:16:56 -0700

    added navbar and useritme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Useritem from './components/users/Useritem';
 import './App.css';
 
-class App extends Component{
-  render(){
-  return (
-    <div className='App'>
-      <h1>Hello from React</h1>
-    </div>
-  );
-
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Navbar />
+        <Useritem />
+      </div>
+    );
   }
 }
 export default App;
