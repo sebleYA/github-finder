accb6cba6baba893b4cd732030937b6aa9322edd
sebli <seblewongelabera@gmail.com>
2022-06-15 15:27:37 -0700

    updated codes

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -1,9 +1,9 @@
 import React, { Fragment } from 'react';
 import spinner from './spinner.gif';
-import { Fragment } from 'react';
 
 export const Spinner = () => (
   <Fragment>
     <img src={spinner} alt='Loading...' style={{ width: '200px', margin: 'auto', display: 'block' }} />
   </Fragment>
 );
+export default Spinner
