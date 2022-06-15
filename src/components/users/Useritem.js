96bdc7c39f2a0e8ee0cb5a0ee947d275a8e9e5d4
sebli <seblewongelabera@gmail.com>
2022-06-15 15:25:15 -0700

    updated codes and style

--- src/components/users/Useritem.js
+++ src/components/users/Useritem.js
@@ -1,25 +1,24 @@
-import React  from 'react';
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const Useritem = ({user: { login, avatar_url, html_url}}) => { // using deconstruction method 
-  
-  
-    return (
-      <div className='card text-center'>
-        <img src={avatar_url} alt='' className='round-img' style={{ width: '60px' }} />
-        <h3>{login} </h3>
-        <div>
-          <a href={html_url} className='btn btn-dark btn-sm my-1'>
-            More
-          </a>
-        </div>
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  // using deconstruction method
+
+  return (
+    <div className='card text-center'>
+      <img src={avatar_url} alt='' className='round-img' style={{ width: '60px' }} />
+      <h3>{login} </h3>
+      <div>
+        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+          More
+        </a>
       </div>
-    );
-  
-}
+    </div>
+  );
+};
 
-Useritem.protoType = {
+UserItem.PropType = {
   user: PropTypes.object.isRequired
-}
+};
 
-export default Useritem;
+export default UserItem;
