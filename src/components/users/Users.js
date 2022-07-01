241c07f0e30b2c5d78682158db7e8073586cd403
sebli <seblewongelabera@gmail.com>
2022-07-01 14:15:26 -0700

    cleared by removing the props and adding the githubContext

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext} from 'react';
 import Useritem from './Useritem';
 import Spinner from '../layout/Spinner';
-import PropTypes from 'prop-types';
+import GithubContext from '../../context/github/githubContext';
 
-const Users = ({ users, loading }) => {
+const Users = () => {
+  const githubContext = useContext(GithubContext)
+
+const {users, loading} = githubContext
+
   if (loading) {
     return <Spinner />;
   } else {
@@ -17,10 +21,7 @@ const Users = ({ users, loading }) => {
   }
 };
 
-Users.prototype = {
-  users: PropTypes.array.isRequired,
-  loading: PropTypes.bool.isRequired
-};
+
 
 const userStyle = {
   display: 'grid',
