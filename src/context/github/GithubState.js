dbd40fca52f907a2d6bf4449fbd2064f9e398421
sebli <seblewongelabera@gmail.com>
2022-07-01 13:19:05 -0700

    updated by adding searchUsers function and dispatch

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -1,20 +1,38 @@
-import React, { useReducer } from 'react';
-import axios from 'axios';
-import GithubContext from './githubContext';
-import GithubReducer from './githubReducer';
-import { SEARCH_USERS, SET_LOADING, CLEAR_USERS, GET_USER, GET_REPOS } from '../types';
+import React, { useReducer } from "react";
+import axios from "axios";
+import GithubContext from "./githubContext";
+import GithubReducer from "./githubReducer";
+import {
+  SEARCH_USERS,
+  SET_LOADING,
+  CLEAR_USERS,
+  GET_USER,
+  GET_REPOS,
+} from "../types";
 
 const GithubState = (props) => {
   const initialState = {
     users: [],
     user: {},
     repos: [],
-    loading: false
+    loading: false,
   };
 
   const [state, dispatch] = useReducer(GithubReducer, initialState);
 
-  // Search Users
+  // Search Github users
+  const searchUsers = async (text) => {
+    setLoading();
+
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_seccret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({
+      type: SEARCH_USERS,
+      payload: res.data.items,
+    });
+  };
 
   // Get User
 
@@ -24,9 +42,17 @@ const GithubState = (props) => {
 
   // Set Loasing
 
+  const setLoading = () => dispatch({ type: SET_LOADING });
+
   return (
     <GithubContext.Provider
-      value={{ users: state.users, user: state.user, repos: state.repos, loading: state.loading }}
+      value={{
+        users: state.users,
+        user: state.user,
+        repos: state.repos,
+        loading: state.loading,
+        searchUsers,
+      }}
     >
       {props.children}
     </GithubContext.Provider>
