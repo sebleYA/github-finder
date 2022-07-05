f585f645082b6cfba06f9ee1bc195ad2b825ec9e
sebli <seblewongelabera@gmail.com>
2022-07-04 19:44:43 -0700

    added case for a single user

--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -7,6 +7,7 @@ import {
 } from "../types";
 
 export default (state, action) => {
+    console.log(action);
   switch (action.type) {
     case SEARCH_USERS:
       return {
@@ -14,21 +15,20 @@ export default (state, action) => {
         users: action.payload,
         loading: false,
       };
-      case GET_USER:
-        return{
-            ...state,
-            user: action.payload,
-            loading: false
-        
+    case GET_USER:
+      return {
+        ...state,
+        user: action.payload,
+        loading: false,
       };
-      case CLEAR_USERS:{
-        return {
-            ...state,
-            users: [],
-            loadin: false
-        }
+    case CLEAR_USERS: {
+      return {
+        ...state,
+        users: [],
+        loadin: false,
       };
-   
+    }
+
     case SET_LOADING:
       return {
         ...state,
