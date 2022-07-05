4dc8a7d6aba2485ee86bb7b0037d0f9df40a399d
sebli <seblewongelabera@gmail.com>
2022-07-04 20:10:16 -0700

    cleaned app.js by removing all the methods

--- src/App.js
+++ src/App.js
@@ -6,14 +6,12 @@ import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
-import axios from 'axios';
+
 
 import GithubState from './context/github/GithubState';
 import './App.css';
 
 const App = () => {
-  const [repos, setRepos] = useState([]);
-  const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
 
   // async componentDidMount() {
@@ -25,24 +23,6 @@ const App = () => {
 
   //   this.setState({ users: res.data, loading: false });
   // }
-
-  
-
-  
-
-  // Get users repo
-
-  const getUserRepos = async (username) => {
-    setLoading(true);
-
-    const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_seccret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-
-    setRepos(res.data);
-    setLoading(false);
-  };
-
 
   // set Alert
   const showAlert = (msg, type) => {
@@ -74,16 +54,7 @@ const App = () => {
             <Route
               exact
               path={'/user/:login'}
-              render={(props) => (
-                <User
-                  {...props}
-                 
-                  getUserRepos={getUserRepos}
-                 
-                  repos={repos}
-                  
-                />
-              )}
+            component={User}
             />
           </Switch>
         </div>
