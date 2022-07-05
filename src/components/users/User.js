ba5089dcdb06220ad7d658353fbde153b74e1ad2
sebli <seblewongelabera@gmail.com>
2022-07-04 20:11:19 -0700

    repos and getuserRepos props were deleted

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,13 +1,14 @@
 import React, { Fragment, useEffect, useContext } from 'react';
 import Spinner from '../layout/Spinner';
 import Repos from '../repos/Repos';
-import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import GithubContext from '../../context/github/githubContext';
 
-const User = ({  repos, getUserRepos, match }) => {
+const User = ({ match }) => {
   const githubContext = useContext(GithubContext)
-  const{user, getUser, loading} = githubContext
+
+
+  const{user, getUser, loading, repos, getUserRepos} = githubContext
 
   useEffect(() => {
     getUser(match.params.login);
@@ -93,9 +94,6 @@ const User = ({  repos, getUserRepos, match }) => {
   );
 };
 
-User.propTypes = {
-  repos: PropTypes.array.isRequired,
-  getUserRepos: PropTypes.func.isRequired
-};
+
 
 export default User;
